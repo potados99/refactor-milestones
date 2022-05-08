eadb415d8dffcde875508a99f4dbb681bff36c84
potados99 <potados99@gmail.com>
2022-05-08 15:03:21 +0900

    chore: shows usage on exit when error is caught

--- cli.js
+++ cli.js
@@ -4,11 +4,17 @@ const { run } = require("./index");
 const { program } = require("commander");
 
 const args = program
-  .option("-p, --prefix [string]", "Path to project")
-  .option("-v, --verbose", "Print all information")
+  .showSuggestionAfterError(true)
+  .showHelpAfterError(true)
+  .option("-p, --prefix [string]", "path that will be used as cwd")
+  .option("-v, --verbose", "print all information")
   .parse()
   .opts();
 
 (async function () {
-  await run(args);
+  try {
+    await run(args);
+  } catch (e) {
+    program.error(e.message);
+  }
 })();
